8cf7f407fac855c8324ba6b16aecb44049f43f02
JamilJalal <jamil.jalal94@gmail.com>
2018-10-27 22:07:05 -0400

    Last Update 10:06 pm

--- HivPrevension/js/custom.js
+++ HivPrevension/js/custom.js
@@ -19,7 +19,9 @@
     $('body').vegas({
         slides: [
             { src: 'images/slide-image01.jpg' },
-            { src: 'images/slide-image02.jpg' }
+            { src: 'images/slide-image02.jpg' },
+            { src: 'images/slide-image03.jpg' },
+            { src: 'images/slide-image04.jpg' }
         ],
         timer: false,
         transition: [ 'zoomOut', ]
